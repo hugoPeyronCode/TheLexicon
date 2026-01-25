4d3ec0ae15ad435705e6f65a18e5e5009ade7d2d
hugoPeyronCode <hugopeyron@gmail.com>
2026-01-25 13:41:26 +0100

    Initial Commit

---
+++ TheLexicon/TheLexiconApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheLexiconApp.swift
+//  TheLexicon
+//
+//  Created by Hugo Peyron on 25/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct TheLexiconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
